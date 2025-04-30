8cc361e64560936a5c60972e61ae407b5744aaf7
edw4rdkk <eduardkyrychenko923@gmail.com>
2025-04-30 22:55:17 +0300

    fetch spotify profile and upsert User document

---
+++ src/models.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  spotifyId: { type: String, unique: true, required: true },
+  displayName: String,
+  accessToken: String,
+  refreshToken: String,
+});
+
+const playSchema = new mongoose.Schema({
+  trackId: String,
+  playedAt: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+const User = mongoose.model("User", userSchema);
+const Play = mongoose.model("Play", playSchema);
+
+module.exports = { User, Play };
