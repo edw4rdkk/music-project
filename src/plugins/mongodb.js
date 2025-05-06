82fc8e3eb9d8b7cc499c2d02d2389c6b8151021d
edw4rdkk <eduardkyrychenko923@gmail.com>
2025-05-06 12:35:00 +0300

    add onClose hook to disconnect Mongoose

--- src/plugins/mongodb.js
+++ src/plugins/mongodb.js
@@ -5,6 +5,11 @@ module.exports = fp(async (fastify, opts) => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     fastify.decorate("mongoose", mongoose);
+    fastify.addHook("onClose", async (instance, done) => {
+      await mongoose.disconnect();
+      fastify.log.info("mongodb disconnected");
+      done();
+    });
     fastify.log.info("mongodb connected");
   } catch (err) {
     fastify.log.error(err);
