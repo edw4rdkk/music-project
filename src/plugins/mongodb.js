18b0b263fc7c98031909c4da6d200d7a9fbd25aa
edw4rdkk <eduardkyrychenko923@gmail.com>
2025-05-06 16:03:40 +0300

    fix onClose hook

--- src/plugins/mongodb.js
+++ src/plugins/mongodb.js
@@ -2,17 +2,12 @@ const fp = require("fastify-plugin");
 const mongoose = require("mongoose");
 
 module.exports = fp(async (fastify, opts) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    fastify.decorate("mongoose", mongoose);
-    fastify.addHook("onClose", async (instance, done) => {
-      await mongoose.disconnect();
-      fastify.log.info("mongodb disconnected");
-      done();
-    });
-    fastify.log.info("mongodb connected");
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+  await mongoose.connect(process.env.MONGODB_URI);
+  fastify.decorate("mongoose", mongoose);
+  fastify.log.info("mongodb connected");
+
+  fastify.addHook("onClose", async () => {
+    await mongoose.disconnect();
+    fastify.log.info("mongodb disconnected");
+  });
 });
